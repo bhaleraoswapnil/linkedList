9597648df994ac9f18973cb73570896bea08ceed
swapnil <bhalerao.swapnil11@gmail.com>
2021-09-15 20:23:05 +0530

    6 : Sum List

--- linkedList/main.cpp
+++ linkedList/main.cpp
@@ -4,11 +4,10 @@
 //
 //  Created by Swapnil Bhalerao on 14/09/21.
 //  clang++ -std=c++14 -stdlib=libc++ main.cpp
-//  Question : Delete given Node in Singly Linked List
-//  i/p: 1->2->3->4->5 & given node 3.
-//  o/p: 1->2->4->5
+//  Question : Sum List
+//  i/p: (7->1->6) + (5->9->2) that is, 617+295 = 912
+//  o/p: 2->1->9
 #include <iostream>
-#include <unordered_set>
 using namespace std;
 struct Node
 {
@@ -27,11 +26,13 @@ private:
 public:
     LinkedList();
     ~LinkedList();
-    void createLinkList();
     void deleteLinkList();
     void printLL();
-    void DeleteGivenNode(Node *);
-    Node *ReturnNode(int);
+    void createLinkList(int);
+    void AppendNode(int);
+    void sumList(Node *, Node *);
+    void copyLinkedList(Node *);
+    Node *getHeadPtr() { return head; }
 };
 LinkedList::LinkedList() : head(nullptr), tail(nullptr)
 {
@@ -39,41 +40,6 @@ LinkedList::LinkedList() : head(nullptr), tail(nullptr)
 LinkedList::~LinkedList()
 {
     deleteLinkList();
-}
-Node *LinkedList::ReturnNode(int pos)
-{
-    Node *p = head;
-    int len = 0;
-    while (p != nullptr)
-    {
-        len++;
-        p = p->next;
-    }
-    if (pos >= len || pos <= 0)
-    {
-        return nullptr;
-    }
-    else
-    {
-        p = head;
-        for (int i = 0; i < pos - 1; i++)
-        {
-            p = p->next;
-        }
-        return p;
-    }
-}
-void LinkedList::DeleteGivenNode(Node *p)
-{
-    Node *r = p;
-    while (p->next != nullptr)
-    {
-        p->data = p->next->data;
-        r = p;
-        p = p->next;
-    }
-    r->next = nullptr;
-    delete p;
 }
 void LinkedList::printLL()
 {
@@ -86,23 +52,91 @@ void LinkedList::printLL()
     }
     cout << "NULL" << endl;
 }
-void LinkedList::createLinkList()
+void LinkedList::copyLinkedList(Node *p)
 {
-    int num = 0;
-    cout << "Enter Linked List length\n";
-    cin >> num;
-    int val = 0;
-    cout << "Enter data\n";
-    cin >> val;
-    head = new Node(val);
-    tail = head;
-    for (int i = 0; i < num - 1; i++)
+    if (nullptr != p)
     {
-        cout << "Enter data\n";
-        cin >> val;
-        Node *temp = new Node(val);
-        tail->next = temp;
-        tail = temp;
+        while (p != nullptr)
+        {
+            AppendNode(p->data);
+            p = p->next;
+        }
+    }
+    else
+    {
+        cout << "NULL pointer head" << endl;
+    }
+}
+void LinkedList::sumList(Node *p1, Node *p2)
+{
+    if (p1 == nullptr && p2 == nullptr)
+    {
+        cout << "Invalid Lisked Lists" << endl;
+    }
+    else if (p1 == nullptr)
+    {
+        copyLinkedList(p2);
+        cout << "same as p2 Linked List" << endl;
+    }
+    else if (p2 == nullptr)
+    {
+        copyLinkedList(p1);
+        cout << "same as p1 Linked List" << endl;
+    }
+    else
+    {
+        int carry = 0;
+        int val = 0;
+        while (p1 != nullptr || p2 != nullptr)
+        {
+            if (p1 != nullptr && p2 != nullptr)
+            {
+                val = p1->data + p2->data + carry;
+                p1 = p1->next;
+                p2 = p2->next;
+            }
+            else if (p1 != nullptr)
+            {
+                val = p1->data + carry;
+                p1 = p1->next;
+            }
+            else
+            {
+                val = p2->data + carry;
+                p2 = p2->next;
+            }
+            carry = val / 10;
+            val = val % 10;
+            AppendNode(val);
+        }
+        if (carry)
+        {
+            AppendNode(carry);
+        }
+    }
+}
+void LinkedList::AppendNode(int num)
+{
+    if (head == nullptr)
+    {
+        head = new Node(num);
+        tail = head;
+        return;
+    }
+    else
+    {
+        Node *p = new Node(num);
+        tail->next = p;
+        tail = tail->next;
+    }
+}
+void LinkedList::createLinkList(int num)
+{
+    while (num != 0)
+    {
+        int val = num % 10;
+        num = num / 10;
+        AppendNode(val);
     }
 }
 void LinkedList::deleteLinkList()
@@ -113,7 +147,7 @@ void LinkedList::deleteLinkList()
         Node *temp2 = head->next;
         while (temp1 != nullptr)
         {
-            cout << "deleting node: " << temp1->data << "\n";
+            // cout << "deleting node: " << temp1->data << "\n";
             delete temp1;
             temp1 = temp2;
             if (temp2 != nullptr)
@@ -128,22 +162,22 @@ void LinkedList::deleteLinkList()
 int main(int argc, const char *argv[])
 {
     // insert code here...
-    LinkedList list;
-    list.createLinkList();
-    list.printLL();
-    int pos = 0;
-    cout << "Enter node position" << endl;
-    cin >> pos;
-    Node *p = list.ReturnNode(pos);
-    if (p != nullptr)
-    {
-        list.DeleteGivenNode(p);
-    }
-    else
-    {
-        cout << "Invalid Node position\n";
-    }
-    list.printLL();
-    list.deleteLinkList();
+    LinkedList L1;
+    cout << "Enter 1st number" << endl;
+    int num = 0;
+    cin >> num;
+    L1.createLinkList(num);
+    L1.printLL();
+
+    LinkedList L2;
+    cout << "Enter 2st number" << endl;
+    cin >> num;
+    L2.createLinkList(num);
+    L2.printLL();
+
+    LinkedList L3;
+    L3.sumList(L1.getHeadPtr(), L2.getHeadPtr());
+    cout << "\n\n";
+    L3.printLL();
     return 0;
 }
