1a47857c7bab68641a2ed00efa36f651176e149c
swapnil <bhalerao.swapnil11@gmail.com>
2021-09-16 07:06:44 +0530

    8: Intersetion - Determine two Linked list interset and return interset node.

--- linkedList/main.cpp
+++ linkedList/main.cpp
@@ -4,10 +4,9 @@
 //
 //  Created by Swapnil Bhalerao on 14/09/21.
 //  clang++ -std=c++14 -stdlib=libc++ main.cpp
-//  Question : Palindrome - Write function to check linked list is palindrome or not.
+//  Question : Intersetion - Determine two Linked list interset and return interset node.
 
 #include <iostream>
-#include <stack>
 using namespace std;
 struct Node
 {
@@ -22,52 +21,44 @@ class LinkedList
 private:
     Node *head;
     Node *tail;
+    void deleteLinkList();
 
 public:
     LinkedList();
     ~LinkedList();
     void createLinkList();
-    void deleteLinkList();
     void printLL();
-    bool checkPalindrome();
+    Node *getIntersectionPointer();
+    void jointLL(Node *);
+    int getLength();
+    Node *getHeadPtr() { return head; }
+    Node *getTailPtr() { return tail; }
 };
 LinkedList::LinkedList() : head(nullptr), tail(nullptr)
 {
 }
 LinkedList::~LinkedList()
 {
-    deleteLinkList();
+    //    deleteLinkList();
 }
-bool LinkedList::checkPalindrome()
+int LinkedList::getLength()
 {
-    // 1-2-3-3-2-1 ==> fast = null EVEN
-    //s=1,f=1 st=1 |    s=2 f=3 st= 2,1    |    s=3 f=2 st=3 2 1    |    s=3 f=null break loop
-    // 1-2-3-4-3-2-1 ==> fast->next=null ODD
-    // s=1,f=1 st=1==> s=2,f=3 st=2 1 ==> s=3,f=3 st= 3 2 1 ==> s=4,f=1 st=f->next=null break loop
-    std::stack<int> st;
-    Node *slow = head;
-    Node *fast = head;
-    while (fast != nullptr && fast->next != nullptr)
+    Node *p = head;
+    int len = 0;
+    while (p != nullptr)
     {
-        st.push(slow->data);
-        slow = slow->next;
-        fast = fast->next->next;
+        len++;
+        p = p->next;
     }
-
-    if (fast != nullptr && fast->next == nullptr)
-    {
-        slow = slow->next;
-    }
-    while (!st.empty())
-    {
-        if (st.top() != slow->data)
-        {
-            return false;
-        }
-        st.pop();
-        slow = slow->next;
-    }
-    return true;
+    return len;
+}
+void LinkedList::jointLL(Node *p)
+{
+    tail->next = p;
+}
+Node *LinkedList::getIntersectionPointer()
+{
+    return head->next->next->next;
 }
 void LinkedList::printLL()
 {
@@ -107,7 +98,7 @@ void LinkedList::deleteLinkList()
         Node *temp2 = head->next;
         while (temp1 != nullptr)
         {
-            cout << "deleting node: " << temp1->data << "\n";
+            //            cout << "deleting node: " << temp1->data << "\n";
             delete temp1;
             temp1 = temp2;
             if (temp2 != nullptr)
@@ -119,19 +110,83 @@ void LinkedList::deleteLinkList()
     }
 }
 
-int main(int argc, const char *argv[])
+bool findIntersecion(LinkedList &L1, LinkedList &L2)
 {
-    // insert code here...
-    LinkedList list;
-    list.createLinkList();
-    list.printLL();
-    if (list.checkPalindrome())
+    int len1 = L1.getLength();
+    int len2 = L2.getLength();
+    Node *p1 = L1.getHeadPtr();
+    Node *p2 = L2.getHeadPtr();
+    if (p1 == nullptr || p2 == nullptr)
     {
-        cout << "Linked list is Palindrome" << endl;
+        return false;
+    }
+    // 1->2->3->4->5->6 L1=6
+    //10->20->4->5->6   L2=5
+    // L1-L2 = 1
+    // move L1 by 1 step and then start compare.
+    if (len1 > len2)
+    {
+        int diff = len1 - len2;
+        while (diff > 0)
+        {
+            diff--;
+            p1 = p1->next;
+        }
+    }
+    else if (len2 > len1)
+    {
+        int diff = len2 - len1;
+        while (diff > 0)
+        {
+            diff--;
+            p2 = p2->next;
+        }
     }
     else
     {
-        cout << "Linked list is Not Palindrome" << endl;
+        if (L1.getTailPtr() == L2.getTailPtr())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
+
+    while (p1 != nullptr && p2 != nullptr)
+    {
+        if (p1 == p2)
+        {
+            return true;
+        }
+        p1 = p1->next;
+        p2 = p2->next;
+    }
+    return false;
+}
+int main(int argc, const char *argv[])
+{
+    // insert code here...
+    LinkedList L1;
+    L1.createLinkList();
+    L1.printLL();
+
+    LinkedList L2;
+    L2.createLinkList();
+    L2.printLL();
+
+    L2.jointLL(L1.getIntersectionPointer());
+    L2.printLL();
+
+    if (findIntersecion(L1, L2))
+    {
+        cout << "True" << endl;
+    }
+    else
+    {
+        cout << "False" << endl;
+    }
+
     return 0;
 }
